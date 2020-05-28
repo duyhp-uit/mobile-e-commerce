e3a4131901b0b4587fb1ff8952f3f5308bc2b171
16520285 <kudophuongduy@gmail.com>
2020-05-28 14:28:17 +0700

    Config connection to MySQL

--- leads/models.py
+++ leads/models.py
@@ -5,3 +5,14 @@ class Lead(models.Model):
     email = models.EmailField()
     message = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class Product(models.Model):
+    name= models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    regular_price = models.IntegerField()
+    discount_price = models.IntegerField()
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
